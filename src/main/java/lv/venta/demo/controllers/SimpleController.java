57fe940633a10bf30a896e1b5fba5bbce17b4d96
s20straalek <s20straalek@vea.lv>
2022-04-17 18:51:27 +0300

    Edit: add controller with product arraylist sending

--- src/main/java/lv/venta/demo/controllers/SimpleController.java
+++ src/main/java/lv/venta/demo/controllers/SimpleController.java
@@ -1,5 +1,8 @@
 package lv.venta.demo.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +11,11 @@ import lv.venta.demo.model.Product;
 
 @Controller
 public class SimpleController {
+    private ArrayList<Product> allProducts = new ArrayList<>(
+        Arrays.asList(
+            new Product("Maize", 2.13f, 3),
+            new Product("Udens", 0.56f, 100),
+            new Product("Banans", 0.35f, 21)));
     /**
      * uzsaucot url localhost:8080/simple browserī tiks izvadīts teksts
      * home.html lapa
@@ -28,7 +36,15 @@ public class SimpleController {
     public String productFunc(Model model) {
         Product product = new Product("Abols", 0.99f, 10);
         model.addAttribute("package", product);
-        System.out.println("product kontrolieris ir izsaukts");
+        System.out.println("Produktu kontrolieris izsaukts");
         return "product-page";// ieladejam product-page.html lapu ar package sutijumu
+    }
+    //localhost:8080/list
+    //@requestParam url-> //all-product?id=1
+    @GetMapping("/list")
+    public String listFunc(Model model) {
+        model.addAttribute("package", allProducts);
+        System.out.println("Produktu kontrolieris izsaukts");
+        return "all-product-page";
     }
 }

385e85d965cd1eb7581eeefa343d6e4b4620b938
s20straalek <s20straalek@vea.lv>
2022-04-21 13:37:05 +0300

    Add: Product CRUD service as class with create function

---
+++ src/main/java/lv/venta/demo/services/impl/CRUDProductServiceImpl.java
@@ -0,0 +1,59 @@
+package lv.venta.demo.services.impl;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.stereotype.Service;
+
+import lv.venta.demo.model.Product;
+import lv.venta.demo.services.ICRUDProductService;
+
+@Service
+public class CRUDProductServiceImpl implements ICRUDProductService {
+
+    private ArrayList<Product> allProducts = new ArrayList<>(
+        Arrays.asList(
+            new Product("Maize", 2.13f, 3),
+            new Product("Udens", 0.56f, 100),
+            new Product("Banans", 0.35f, 21)));
+
+    @Override
+    public Product createProduct(Product temp) {
+        for(Product prod: allProducts) {
+            if(prod.getTitle().equals(temp.getTitle()) && prod.getPrice() == temp.getPrice()) {
+                int newQuantity = prod.getQuantity() + temp.getQuantity();
+                prod.setQuantity(newQuantity);
+                return prod;
+            }
+        }
+        Product newProduct = new Product(temp.getTitle(), temp.getPrice(), temp.getQuantity());
+        allProducts.add(newProduct);
+        return newProduct;
+
+    }
+
+    @Override
+    public ArrayList<Product> readAllProduct() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Product readById(int id) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void updateById(int id, Product temp) throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void deleteById(int id) throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}

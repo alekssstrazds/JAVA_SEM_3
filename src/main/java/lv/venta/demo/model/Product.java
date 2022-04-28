be48879799691b8a10c3076ca7be5bef4c6f2c3c
s20straalek <s20straalek@vea.lv>
2022-04-28 15:49:19 +0300

    Add: add DataBase annotation to Product

--- src/main/java/lv/venta/demo/model/Product.java
+++ src/main/java/lv/venta/demo/model/Product.java
@@ -1,23 +1,35 @@
 package lv.venta.demo.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+@Table
+@Entity
 public class Product {
+    @Column(name = "Title")
     @Size(min = 3, max = 30, message = "Burtu skaits nedrīkst būt mazāks par 3 un lielāks par 30.")
     @Pattern(regexp = "[A-Z]{1}[a-z]+", message = "Pirmam burtam jābūt lielam un citiem maziem.")
     private String title;
+    @Column(name = "Price")
     @Min(0)
     @Max(10000)
     private float price;
+    @Column(name = "Quantity")
     @Min(0)
     @Max(10000)
     private int quantity;
+    @Column(name="Id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private int id;
-
-    private static int counter = 0;
 
     public String getTitle() {
         return title;
@@ -41,15 +53,10 @@ public class Product {
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
-    public void setId() {
-        this.id = counter;
-        counter++;
-    }
-
+    
     public Product() {  
     }
     public Product(String title, float price, int quantity) {
-        setId();
         this.title = title;
         this.price = price;
         this.quantity = quantity;
